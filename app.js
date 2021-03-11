b0326b7acca8600901d8a362e9ef09b5d23e8496
Aarani Sivasekaram <aarani.sivasekaram@gmail.com>
2021-03-10 21:32:33 -0500

    Updated app.js file to include table functions
    Imported data, created table, and iterated through data and placed within table rows and cells

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+// Creat function for building table
+function buildTable(data) {
+    // Clear out existing data
+    tbody.html("");
+    // Loop through each object in the data and append each value in a row
+    data.forEach((dataRow) => {
+      let row = tbody.append("tr");
+      // Loop through each field and add each value as a table cell
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+  });
+}
+
